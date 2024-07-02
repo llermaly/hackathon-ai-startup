259b7f4d01253a83b90e3af6936e44e559cf3792
abstract829 <maxxii.1820@gmail.com>
2024-07-02 17:07:31 -0400

    improvements

--- frontend/renderer/components/side-chat.tsx
+++ frontend/renderer/components/side-chat.tsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { exampleActions } from "./actions";
 import { FaRegTrashAlt } from "react-icons/fa";
 import { scenarioClassName, scenarioImg } from "./chat";
+import { VscGithubAction } from "react-icons/vsc";
 
 interface SideChatProps {
   handleSubmitMessage: (message: string) => void;
@@ -32,25 +33,27 @@ const ActionItem = ({
 }) => {
   return (
     <div
-      className={`mb-4 w-full transform bg-gray-100 rounded-md cursor-pointer entry ${className}`}
+      className={`mb-4 w-full transform bg-gray-100 rounded-md cursor-pointer entry gradient-blue `}
       {...rest}
     >
-      <div className="flex justify-end px-3 pt-3">
-        <FaRegTrashAlt
-          className="text-red-500 cursor-pointer"
-          onClick={(e) => {
-            e.stopPropagation();
-            onDelete();
-          }}
-        />
-      </div>
-      <div className="px-3 pb-3.5 grid grid-cols-12 gap-x-4">
+      <div className="px-3 pt-3 pb-3.5 grid grid-cols-12 gap-x-4">
         <div className="flex items-center justify-center col-span-2">
-          <img src={image} className="w-10 h-10 rounded-md" />
+          <VscGithubAction className="w-10 h-10 text-blue-400" />
+          {/* <img src={image} className="w-10 h-10 rounded-md" /> */}
         </div>
         <div className="col-span-10 break-words">
-          <span className="text-sm font-semibold text-gray-600">{name}</span>
-          <p className="text-xs text-gray2">{text}</p>
+          <div className="flex items-start justify-between">
+            <span className="text-sm font-semibold text-gray-600">{name}</span>
+
+            <FaRegTrashAlt
+              className="text-gray-900 cursor-pointer"
+              onClick={(e) => {
+                e.stopPropagation();
+                onDelete();
+              }}
+            />
+          </div>
+          <p className="mt-1 text-xs text-gray2">{text}</p>
         </div>
       </div>
     </div>
@@ -69,8 +72,6 @@ const SideChat = (props: SideChatProps) => {
     name: "",
     text: "",
     prompt: "",
-    image: "",
-    className: "",
   });
 
   useEffect(() => {
@@ -89,18 +90,12 @@ const SideChat = (props: SideChatProps) => {
     const currentActions = localStorage.getItem("quickActions");
     const actions = currentActions ? JSON.parse(currentActions) : [];
 
-    const newAction = {
-      ...form,
-      image: scenarioImg[form.image],
-      className: scenarioClassName[form.image],
-    };
-
-    actions.push(newAction);
+    actions.push(form);
 
     localStorage.setItem("quickActions", JSON.stringify(actions));
     setQuickActions(actions);
 
-    setForm({ name: "", text: "", prompt: "", image: "", className: "" });
+    setForm({ name: "", text: "", prompt: "" });
     setShowModal(false);
   };
 
@@ -188,21 +183,10 @@ const SideChat = (props: SideChatProps) => {
               rows={3}
               onChange={(e) => setForm({ ...form, prompt: e.target.value })}
             />
-            <select
-              onChange={(e) => setForm({ ...form, image: e.target.value })}
-              className="w-full p-2 mt-2 text-sm bg-transparent border rounded-md"
-            >
-              <option value="">Select image</option>
-              <option value="monday">Monday</option>
-              <option value="slack">Slack</option>
-              <option value="email">Email</option>
-              <option value="stripe">Stripe</option>
-            </select>
+
             <div className="flex gap-8">
               <button
-                disabled={
-                  !form.name || !form.text || !form.prompt || !form.image
-                }
+                disabled={!form.name || !form.text || !form.prompt}
                 className="w-2/3 px-4 py-2 mt-4 mb-4 text-sm text-white transform bg-blue-600 rounded-md cursor-pointer disabled:hover:scale-100 disabled:cursor-default disabled:bg-gray-400 active:bg-opacity-80"
                 onClick={handleCreateAction}
               >
